7175b12b488418294add91e767fb02931778da5e
Adam Kruchkow <krucha@rpi.edu>
2015-11-10 16:44:48 -0500

    Began month functions.

---
+++ monthFunctions.js
@@ -0,0 +1,14 @@
+// Data for months
+var months;
+months[0] = ["January",31];
+months[1] = ["February",28];
+months[2] = ["March",31];
+months[3] = ["Arpil",30];
+months[4] = ["May",31];
+months[5] = ["June",30];
+months[6] = ["July",31];
+months[7] = ["August",31];
+months[8] = ["September",30];
+months[9] = ["October",31];
+months[10] = ["November",30];
+months[11] = ["December",31];
