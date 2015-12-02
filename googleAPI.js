1f5185d39d5a5a545413f694131a8af0a6cfc7b7
Adam Kruchkow <krucha@rpi.edu>
2015-12-02 12:02:58 -0500

    Google API

---
+++ googleAPI.js
@@ -0,0 +1,95 @@
+// Your Client ID can be retrieved from your project in the Google
+// Developer Console, https://console.developers.google.com
+var CLIENT_ID = '483530694362-2e8fb2sgqndnsprm63nip0dnmk55dn41.apps.googleusercontent.com';
+
+var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+/**
+ * Check if current user has authorized this application.
+ */
+function checkAuth() {
+  gapi.auth.authorize(
+    {
+      'client_id': CLIENT_ID,
+      'scope': SCOPES.join(' '),
+      'immediate': true
+    }, handleAuthResult);
+}
+
+/**
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    // Hide auth UI, then load client library.
+    loadCalendarApi();
+  }
+}
+
+/**
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
+function handleAuthClick(event) {
+  gapi.auth.authorize(
+    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+    handleAuthResult);
+  return false;
+}
+
+/**
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
+function loadCalendarApi() {
+  gapi.client.load('calendar', 'v3', listUpcomingEvents);
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+  var request = gapi.client.calendar.events.list({
+    'calendarId': 'primary',
+    'timeMin': (new Date(currentYear,currentMonth,1)).toISOString(),
+    'timeMax': (new Date(currentYear,currentMonth+1,0)).toISOString(),
+    'showDeleted': false,
+    'singleEvents': true,
+    'orderBy': 'startTime'
+  });
+
+  request.execute(function(resp) {
+    var events = resp.items;
+
+    var toReturn = [];
+    
+    if (events.length > 0) {
+      for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        
+        // GET THE DATA WE NEED
+        var startTime = event.start.dateTime;
+        if (!startTime) {
+          startTime = event.start.date;
+        }
+        var endTime = event.end.dateTime;
+        if (!endTime) {
+          endTime = event.end.date;
+        }
+        
+        var title = event.summary;
+        var description = event.description;
+        
+        toReturn.push(new Event(startTime,endTime,title,description));
+      }
+    }
+
+    console.log(toReturn);
+    return toReturn;
+  });
+}
